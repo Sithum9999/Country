798f2da14fbc4ce31df083b6ddd85f8d3c73b15b
Sithum9999 <sankalpasithum51@gmail.com>
2024-11-06 17:12:45 +0530

    Country

---
+++ app.js
@@ -0,0 +1,89 @@
+loadItems();
+
+async function loadItems() {
+    
+    let res = await fetch("https://restcountries.com/v3.1/all");
+    let items = await res.json();
+    let body = "";
+    items.forEach(element => {
+        console.log(element);
+        body+=`
+            <div class="col">
+                <div class="card shadow-sm">
+                  <img src=${element.flags.png} alt="">
+                  <div class="card-body">
+                  <h2 class="fw-bold">${element.name.common}</h2>
+                    <p class="card-text">Capital: ${element.capital}</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                     </div>
+                     <p class="S">Regoin: ${element.region}</p>
+                    <div>Population: ${element.population}</div>                   
+
+                    <div class="btn-group ">
+                    </div>
+                     <button type="button" class="bg-info ms-5 mt-3">View on Google Maps</button>
+
+                  </div>
+                </div>
+              </div>
+        `;
+
+        
+        
+    });
+
+    console.log(body);
+
+    document.getElementById("row").innerHTML=body;
+    
+}
+
+function search_animal() {
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('animals');
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+        }
+        else {
+            x[i].style.display = "list-item";
+        }
+    }
+}
+
+
+
+function searchCountry(){
+    console.log("Search!!");
+    let txtSearch = document.getElementById("txtSearch").value;
+
+    fetch(`https://restcountries.com/v3.1/name/${txtSearch}`)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data);
+        let body="";
+        data.forEach(element => {
+            body+=`
+            
+            <div class="bg-body-tertiary p-5 rounded" style="background-image: url(${element.flags.png}); background-repeat: no-repeat; background-size: cover;">
+          <div class="col-sm-8 py-5 mx-auto">
+            <h1 class="display-5 fw-normal text-white">${element.name.common}</h1>
+            <p class="fs-5 text-white">${element.flags.alt}</p>
+            <h2 class="text-white">${element.capital[0]}</h2>
+            <p>From the top down, you'll see a dark navbar, light navbar and a responsive navbar—each with offcanvases built in. Resize your browser window to the large breakpoint to see the toggle for the offcanvas.</p>
+            <p>
+              <a class="btn btn-primary" href="/docs/5.3/components/navbar/#offcanvas" role="button">Learn more about offcanvas navbars &raquo;</a>
+            </p>
+        </div>
+        </div>
+            
+            `
+        });
+
+        document.getElementById("row").innerHTML=body;
+        
+    })
+    
+}
